c482070aee18bd164a036699cfc27f5bf16271c1
Warlley <warlleysantos199@gmail.com>
2026-05-20 14:31:26 -0300

    Ajuste da main, adicionando a parte 1 que foi retirada

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,48 @@
 package org.example;
 
+import org.example.enums.StatusUsuario;
+import org.example.enums.TipoUsuario;
+import org.example.exception.ValidacaoException;
 import org.example.model.Autor;
 import org.example.model.Categoria;
 import org.example.model.Livro;
+import org.example.model.Usuario;
+import org.example.repository.UsuarioRepository;
 import org.example.service.LivroService;
+import org.example.service.UsuarioService;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
+        /*PARTE 1 - TESTE DE USUÁRIO*/
+
+            UsuarioRepository usuarioRepository = new UsuarioRepository();
+            UsuarioService usuarioService = new UsuarioService(usuarioRepository);
+
+            Usuario usuario = new Usuario();
+            usuario.setNomeCompleto("João da Silva");
+            usuario.setEmail("joao@email.com");
+            usuario.setTelefone("(81) 99999-9999");
+            usuario.setMatricula("2026001");
+            usuario.setTipoUsuario(TipoUsuario.ALUNO);
+            usuario.setStatusUsuario(StatusUsuario.ATIVO);
+
+            try {
+                String mensagem = usuarioService.cadastrarUsuario(usuario);
+                System.out.println(mensagem);
+            } catch (ValidacaoException e) {
+                System.out.println("Erro ao cadastrar usuário: " + e.getMessage());
+            }
+
+
+        //----------------------------------
+
+
+        /*PARTE 2 - CRIAÇÃO DE LIVRO*/
+
         // 1. Criamos a base (Categoria e Autor)
         Categoria cat = new Categoria();
         cat.setNome("Programação");
